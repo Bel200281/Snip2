c442bed41fa24b8fe7dedc4bf59b435e429dec77
Belutkina Elena <belenka@rambler.ru>
2025-05-27 12:05:57 +0300

    Done.MS:task 11-12

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
-
+from MainApp.models import Snippet
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -8,10 +8,14 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
+    context = {'pagename': 'Добавление нового сниппета'}   
     return render(request, 'pages/add_snippet.html', context)
 
 
 def snippets_page(request):
-    context = {'pagename': 'Просмотр сниппетов'}
+    snippets = Snippet.objects.all()
+    context = {
+        'pagename': 'Просмотр сниппетов',
+        'snippets': snippets
+        }
     return render(request, 'pages/view_snippets.html', context)
