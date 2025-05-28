a3e3be51db8a958c7149f25b2a9e58f1694c6f68
Belutkina Elena <belenka@rambler.ru>
2025-05-28 15:04:29 +0300

    Change standart options of SnippetForm

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea, TextInput
 from MainApp.models import Snippet
 from django.core.exceptions import ValidationError
 
@@ -6,15 +6,34 @@ from django.core.exceptions import ValidationError
 
 
 class SnippetForm(ModelForm):
-   class Meta:
-       model = Snippet
-       # Описываем поля, которые будем заполнять в форме
-       fields = ['name', 'lang', 'code']
-    
-def clean_name(self):
-        """"Метод для проверки длины поля <name>"""
-        snippet_name = self.cleaned_data.get('name')
-        if snippet_name is None or len(snippet_name.strip()) < 3:
-               raise ValidationError ("Название должно быть минимум 4 символа.")
-        return snippet_name
+       class Meta:
+             model = Snippet
+             # Описываем поля, которые будем заполнять в форме
+             fields = ['name', 'lang', 'code']
+             labels = {"name": "", "lang": "", "code": ""}
+             widgets = {
+                    "name": TextInput(attrs={
+                           "class": "form-control",
+                           "placeholder": "Название сниппета",
+                           "style": "max-width: 300px"
+
+                     }),
+                     "code": Textarea(attrs={
+                    "placeholder": "Код сниппета",
+                    "rows": 5,
+                    "class": "input-large",
+                    "style": "width: 50% !important; resize: vertical !important;"
+                     }),
+             }
+                          
+
+
+       def clean_name(self):
+              """"Метод для проверки длины поля <name>"""
+              snippet_name = self.cleaned_data.get('name')
+              if snippet_name is None or len(snippet_name.strip()) < 3:
+                      raise ValidationError ("Название должно быть минимум 4 символа.")
+              return snippet_name
+       
+
 
